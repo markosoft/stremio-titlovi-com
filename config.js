37ea678643cedeafd6d6b4c76cb49ab388ee9456
Marko Vuković <markosoft5@hotmail.com>
2021-02-10 22:42:00 +0100

    Fix local secret

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
-const localSecret = require('./.secret/secret.json')
-
 var env = process.argv[2] || 'heroku';
 var config = {};
 
 switch (env) {
     case 'heroku':
-		config.port = process.env.PORT
+		  config.port = process.env.PORT
         config.local = "https://stremio-titlovi-com.herokuapp.com"
         config.username = process.env.username
         config.password = process.env.password
         break;
     case 'local':
-		config.port = 8095
+        var localSecret = require('./.secret/secret.json');
+		    config.port = 8095
         config.local = "http://127.0.0.1:" + config.port
         config.username = localSecret.username
         config.password = localSecret.password
