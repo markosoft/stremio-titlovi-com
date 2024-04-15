c8a287997c8159c37d7eaf15a658f973d8869130
Marko Vuković <vukovmarko@gmail.com>
2024-04-15 20:42:53 +0200

    Migrate to Azure, update dependencies

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
-var env = process.argv[2] || 'heroku';
+var env = process.argv[2] || 'azure';
 var config = {};
 
 switch (env) {
-    case 'heroku':
-		  config.port = process.env.PORT
-        config.local = "https://stremio-titlovi-com.herokuapp.com"
+    case 'azure':
+		config.port = process.env.PORT
+        config.local = "https://stremio-titlovi-com.azurewebsites.net"
         config.username = process.env.username
         config.password = process.env.password
         break;
     case 'local':
         var localSecret = require('./.secret/secret.json');
-		    config.port = 8095
-        config.local = "http://127.0.0.1:" + config.port
-        config.username = localSecret.username
-        config.password = localSecret.password
+		config.port = 8095;
+        config.local = "http://127.0.0.1:" + config.port;
+        config.username = localSecret.username;
+        config.password = localSecret.password;
         break;
 }
 
